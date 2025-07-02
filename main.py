5900266068da372976db24d22703b9acdd0a4dc5
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-07-02 10:49:48 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from wifi import *
+from time import sleep
+import socket
+import server
+
+def main ():
+    connect_wifi()
+    print ("menyalakan server")
+    sleep(2)
+    server.webserver()
+    
+if __name__ == '__main__':
+    main()
