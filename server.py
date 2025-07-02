91a20ba7d222f921e0bb928753d86f282e189169
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-07-02 11:03:15 +0700

    Update server.py

--- server.py
+++ server.py
@@ -3,40 +3,6 @@ import alat
 
 PORT = 661
 
-def get_menu():
-    return (
-        "=== MENU ESP32 ===\n"
-        "1. Matikan lampu\n"
-        "2. Nyalakan lampu\n"
-        "3. Kedip cepat\n"
-        "4. Kedip lambat\n"
-        "5. Shutdown ESP32\n"
-        "0. Keluar\n"
-        "Pilih nomor: "
-    )
-
-def proses_pilihan(pilihan, alat):
-    # alat adalah modul/fungsi yang diimport dari alat.py
-    if pilihan == '1':
-        alat.matikan_lampu()
-        return 'Lampu dimatikan\n'
-    elif pilihan == '2':
-        alat.nyalakan_lampu()
-        return 'Lampu dinyalakan\n'
-    elif pilihan == '3':
-        alat.lampu_led_cepat()
-        return 'Lampu berkedip cepat\n'
-    elif pilihan == '4':
-        alat.lampu_led_lambat()
-        return 'Lampu berkedip lambat\n'
-    elif pilihan == '5':
-        alat.mati()
-        return 'ESP32 akan dimatikan.\n'
-    elif pilihan == '0':
-        return 'Keluar. Terima kasih!\n'
-    else:
-        return 'Pilihan tidak dikenal, coba lagi.\n'
-
 def webserver():
     addr = socket.getaddrinfo('0.0.0.0', PORT)[0][-1]
     s = socket.socket()
