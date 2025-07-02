156d50415bcd633aba74921b0f1b94ed5e3411e2
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2025-07-02 10:44:57 +0700

    Create alat.py

---
+++ alat.py
@@ -0,0 +1,35 @@
+from machine import Pin
+from time import sleep
+from wifi import *
+
+# Set pin lampu sesuai hardware (misal D2 = GPIO2)
+LAMP_PIN = 2
+lampu = Pin(LAMP_PIN, Pin.OUT)
+
+def matikan_lampu():
+    lampu.off()
+    print("Lampu dimatikan")
+
+def nyalakan_lampu():
+    lampu.on()
+    print("Lampu dinyalakan")
+ 
+def mati():
+    print("Mematikan esp32")
+    disconnect_wifi()
+
+def lampu_led_cepat(jumlah_kedip=5):
+    print("LED berkedip cepat")
+    for i in range(jumlah_kedip):
+        lampu.on()
+        sleep(0.1)
+        lampu.off()
+        sleep(0.1)
+
+def lampu_led_lambat(jumlah_kedip=5):
+    print("LED berkedip lambat")
+    for i in range(jumlah_kedip):
+        lampu.on()
+        sleep(0.5)
+        lampu.off()
+        sleep(0.5)
